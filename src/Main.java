ba4e0c23efef41de55fd19f6a28cf53be579091a
Velsorjoti <OdzenNedvijiimii@gmail.com>
2022-12-02 21:48:40 +0400

    Это Домашняя работа №2, от второго декабря.

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println(" Домашняя Работа 2 ");
+        int segmentBeginFist = 110;
+        int segmentEndFist = 302;
+        int segmentBeginSecond = 200;
+        int segmentEndSecond = 415;
+        if (segmentBeginFist <= segmentBeginSecond && segmentEndFist > segmentBeginSecond && segmentEndFist <= segmentEndSecond) {
+            System.out.println(" Границы отрезка пересечения " + segmentBeginSecond + segmentEndFist);
+        }
+        else if (segmentBeginFist >= segmentBeginSecond && segmentEndFist <= segmentEndSecond) {
+            System.out.println(" Границы отрезка пересечения " + segmentBeginFist + segmentEndFist);
+        }
+        else if (segmentBeginFist <= segmentBeginSecond && segmentEndFist > segmentEndSecond) {
+            System.out.println(" Границы отрезка  пересечения " + segmentBeginFist + segmentEndFist);
+        }
+        else if (segmentBeginFist > segmentBeginSecond && segmentEndFist > segmentEndSecond) {
+            System.out.println(" Границы отрезка пересечения " + segmentBeginFist + segmentEndSecond);
+        }
+        else if (segmentBeginFist == segmentEndSecond) {
+            System.out.println(" Точка пересечения двух отрезков " + segmentBeginFist);
+        }
+        else if (segmentEndFist == segmentBeginSecond) {
+            System.out.println(" Точка пересечения двух отрезков " + segmentEndFist);
+        }
+        else if (segmentBeginFist > segmentEndSecond || segmentEndFist < segmentBeginSecond) {
+            System.out.println(" Прямые не имеют точек пересечения. ");
+        }
+    }
+
+}
