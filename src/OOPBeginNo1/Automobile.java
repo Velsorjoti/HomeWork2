aafedbe34b0355721b8271d2c3fb875382b2289f
Velsorjoti <OdzenNedvijiimii@gmail.com>
2022-12-19 21:46:12 +0400

    Это Домашняя работа от 19 декабря, вторая часть.

--- src/OOPBeginNo1/Automobile.java
+++ src/OOPBeginNo1/Automobile.java
@@ -1,12 +1,70 @@
 package OOPBeginNo1;
 
 public class Automobile {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+    private int year;
+    private String country;
+
+    public Automobile(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public  String toString() {
         return "Имя марки:" + brand +"." + " Имя модели:" + model +"." + " Объём двигателя:" + engineVolume +"." + " Цвет автомобиля:" + color +"." + " Год производства:" + year +"." + " Страна производитель:" + country +".";
     }
